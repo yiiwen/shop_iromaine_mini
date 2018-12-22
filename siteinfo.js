d7a143d06ad37e09eb9e248e4b49d47c69d042b7
wangweiwen <wen18826478504@163.com>
2018-12-22 15:40:22 +0800

    修改样式

--- siteinfo.js
+++ siteinfo.js
@@ -1,5 +1,6 @@
 module.exports = {
-  name: "街拍Zone",
-    siteroot: "https://shop.itromaine.com/", // 必填: api地址，结尾要带/
+  name: "街拍Zane",
+  siteroot: "https://shop.itromaine.com/", // 必填: api地址，结尾要带/
+//   siteroot: "http://localhost:8080/", // 必填: api地址，结尾要带/
   uniacid: "10001", // 默认即可，勿填
 };
